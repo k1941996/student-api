6055547394e8e07ef1ddb60d2827ce8c1adf1df0
kshitij saini <kshitizsaini1941996@gmail.com>
2026-02-01 18:31:21 +0530

    server done

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/k1941996/student-api/internal/config"
+)
+
 func main() {
 	// load config
+	cfg := config.MustLoad()
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to student api"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Failed to start server %s", err.Error())
+
+	}
+	fmt.Println("Server started")
+
 	// connect to db
 	// setup router
 	// setup server
